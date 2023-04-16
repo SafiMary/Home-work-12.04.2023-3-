c8cda3aacdbcf4eb0c3c5bbbf5bd13a0426ba9ea
Мария Сафи <safim@yandex.ru>
2023-04-16 20:37:39 +0500

    Add project files.

---
+++ Home work 12.04.2023 (3)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_12._04._2023__3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.WriteLine("Нажмите ENTER  чтобы начать забег по кругу, для выхода нажмите ПРОБЕЛ");
+            do
+            {
+                 key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.Enter:
+                        {
+                            DateTime startTime = DateTime.Now;
+                            Console.WriteLine(startTime.ToString("yyyy.MM.dd HH:mm:ss.ffffff"));
+                            Console.WriteLine("Начало забега по кругу");
+                            Console.ReadKey();
+                            DateTime stopTime = DateTime.Now;
+                            TimeSpan timeSpan = stopTime - startTime;
+                            Console.WriteLine($"\nКруг пробежал за {timeSpan.ToString("ss")} секунд");
+                            Console.ReadKey();
+                            break;
+                        }
+                   
+                }
+            } while (key.Key != ConsoleKey.Spacebar);
+
+        }
+    }
+}
